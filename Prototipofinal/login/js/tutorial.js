a79371207b88fb63760bbfc762c22640ae3ded70
Monique1121 <arantzamonique@gmail.com>
2026-04-29 17:13:45 -0600

    avance pagina web

--- Prototipofinal/login/js/tutorial.js
+++ Prototipofinal/login/js/tutorial.js
@@ -1,38 +1,33 @@
 $(document).ready(function () {
 
-    $("#id1, #id2, #id3, #id4").hide();
-
     let actual = 1;
-    let total = 2;
+    let total = 13;
 
+    $(".info").hide();
     $("#id1").show();
 
+    function cambiar(direccion) {
+
+        let siguiente = actual + direccion;
+
+        if (siguiente > total) siguiente = 1;
+        if (siguiente < 1) siguiente = total;
+
+        $(".info").stop(true, true);
+
+        $("#id" + actual).fadeOut(200, function () {
+            $("#id" + siguiente).fadeIn(300);
+        });
+
+        actual = siguiente;
+    }
+
     $("#siguiente").click(function () {
-
-        $("#id" + actual).fadeOut();
-
-        actual++;
-
-        if (actual > total) {
-            actual = 1;
-        }
-
-        $("#id" + actual).fadeIn();
-
+        cambiar(1);
     });
 
     $("#anterior").click(function () {
-
-        $("#id" + actual).fadeOut();
-
-        actual--;
-
-        if (actual < 1) {
-            actual = total;
-        }
-
-        $("#id" + actual).fadeIn();
-
+        cambiar(-1);
     });
 
 });
